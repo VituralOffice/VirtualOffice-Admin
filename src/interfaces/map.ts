648dc842c5be0a222052e6b6254319dbf66a0e7b
Đức Hoàng <duchoang206h@gmail.com>
2024-06-01 19:43:42 +0700

    feat: add fields

--- src/interfaces/map.ts
+++ src/interfaces/map.ts
@@ -4,6 +4,9 @@ export interface IMap {
   capacity: number;
   totalMeeting: number;
   totalChair: number;
+  totalWhiteboard: number;
+  default: boolean;
+  style: string;
   json: string;
   createdAt: string;
   active: boolean;
